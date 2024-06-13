ab3528a99a2644e8ab7b2bbcaedcb10ece06d494
adilaroisa <adilaroisa688@gmail.com>
2024-06-13 12:04:12 +0700

    membuat void postorder

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -96,8 +96,20 @@ public:
             preorder(ptr->leftchild);
         }
     }
-
-
+    void postorder(Node* ptr)
+    {
+        if (ROOT == nullptr)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
 };
 
 
