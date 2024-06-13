ba264dc9c317f3060a853e6208d0f073bc2f621c
adilaroisa <adilaroisa688@gmail.com>
2024-06-13 11:54:43 +0700

    membuat class node

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+class Node
+{
+public:
+    string info;
+    Node* leftchild;
+    Node* rightchild;
+
+   
+    }
+};
+
+
 
 int main()
 {
-    
+   
 }
-
-
