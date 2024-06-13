257d0db07587ed2b9c847ae82cf7fa6649faaea2
adilaroisa <adilaroisa688@gmail.com>
2024-06-13 11:58:50 +0700

    allocate memory for the new node

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -30,7 +30,7 @@ public:
 
     void insert(string element) // insert a node in the binary search tree
     {
-
+        Node* newNode = new Node(element, nullptr, nullptr); // allocate memory for the new node
     }
 
 };
