626f6a3bf9e49572929e869ecd783aab0a02fd80
adilaroisa <adilaroisa688@gmail.com>
2024-06-13 12:01:41 +0700

    if the value in the data field of the new node is less than that of the

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -44,6 +44,10 @@ public:
             ROOT = newNode; // mark the new node as root
             return; // exit
         }
+        if (element < parent->info) // if the value in the data field of the new node is less than that of the
+        {
+            parent->leftchild = newNode; // make the left child of the parent point to the new node
+        }
 
 
     }
